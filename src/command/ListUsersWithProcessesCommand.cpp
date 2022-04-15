b99067a26d41b9a1fa618986f1b235035299e706
eaxm <30189577+eaxm@users.noreply.github.com>
2022-04-15 18:55:18 +0200

    Added proper table for ListUsersWithProcessesCommand

--- src/command/ListUsersWithProcessesCommand.cpp
+++ src/command/ListUsersWithProcessesCommand.cpp
@@ -9,14 +9,17 @@ void ListUsersWithProcessesCommand::execute() {
     std::vector<Process> procList = Process::getProcessList();
     std::map<std::string, int> userProcessAmountMap;
 
-    for (auto &item : procList){
+    for (auto &item: procList) {
         userProcessAmountMap[item.getUser()]++;
     }
 
 
-
-    for (const auto& [key, value] : userProcessAmountMap) {
-        std::cout << key << " = " << value << std::endl;
+    std::cout << "USERS WITH PROCESSES" << std::endl;
+    std::printf("%s\n", std::string(45, '_').c_str());
+    std::printf("| %-30s| %-10s|\n", "USER", "AMOUNT");
+    for (const auto &[key, value]: userProcessAmountMap) {
+        std::printf("| %-30s| %-10d|\n", key.c_str(), value);
     }
+    std::printf("%s\n", std::string(45, '-').c_str());
 
 }
